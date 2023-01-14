eb5ceb9e63f78a5055ed9adb170abb03509ca38e
HAROONSHAIKH123 <hrrnshaikh@gmail.com>
2023-01-14 19:41:29 +0530

    this is version 1

---
+++ STEPS.py
@@ -0,0 +1,45 @@
+
+# Connect mongo DB
+'''
+a. copy client -> connect -> past -> enter
+'''
+
+# copy data set into directory from github
+'''
+wget https://raw.githubusercontent.com/avnyadav/sensor-fault-detection/main/aps_failure_training_set1.csv
+a. paste into terminal -> enter
+'''
+
+# Update requirements.txt file with all library names
+'''
+wget https://raw.githubusercontent.com/avnyadav/sensor-fault-detection/main/aps_failure_training_set1.csv
+a. paste into terminal -> enter
+'''
+
+# Creat file data_dump.py to dumpdata into mongo DB
+'''
+a. as per file
+'''
+
+# Creat Git repo and commit the all code step by step
+'''
+a. creat repo in git
+
+check origin
+b. git remote -v
+
+c. git remote remove origin
+
+d. git log
+
+e. git remote add origin "paste git link"
+
+f. git push origin main
+
+g. git pull origin main
+
+h. git reset --soft "paste ID ref"
+
+i. git add .
+
+'''
